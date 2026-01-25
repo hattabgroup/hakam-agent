c6f8993fb3823e3a4d54e54504c3470872a2d6e6
Ahmad Hattab <ahmad@hattabgroup.com>
2026-01-25 21:07:34 +0300

    increase the token validity to 1 week

--- auth-service/app/security.py
+++ auth-service/app/security.py
@@ -7,7 +7,7 @@ from passlib.context import CryptContext
 # Configuration
 SECRET_KEY = os.getenv("JWT_SECRET", "supersecretkeyForPhase2DevelopmentOnly")
 ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 10080 # 1 week
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -22,7 +22,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
