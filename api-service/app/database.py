b389e9abb793cdeb15f070957c5881d7e0ffb893
Ahmad Hattab <ahmad@hattabgroup.com>
2026-02-14 21:24:49 +0300

    fix db connection issue

--- api-service/app/database.py
+++ api-service/app/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 # API Service Logic Database (can be same physical DB as auth, different tables)
 DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://hakam_user:hakam_password@db/hakam_db")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_recycle=3600, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- auth-service/app/database.py
+++ auth-service/app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://hakam_user:hakam_password@db/hakam_db")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_recycle=3600, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
